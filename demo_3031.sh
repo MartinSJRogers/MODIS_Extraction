ae680094a58a0936e5a5b1147675f083d1b6f8e6
Martin Rogers <43956226+MartinSJRogers@users.noreply.github.com>
2022-03-04 16:26:45 +0000

    Add files via upload

---
+++ demo_3031.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gdal_translate -of GTiff -projwin -2595058 2343705 -309990 795111 bands367_3031.xml Weddell_3031.tiff
---
+++ demo_latlong.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gdal_translate -of GTiff -projwin -64.1 -61.03 -11.8 -78.41 bands367_latlong.xml Weddle_4326.tiff
